44eb97b95ecf71371cecf3684b837309d35c0a1f
Hemonesh Maheshwari <ram123lion24@gmail.com>
2026-07-14 14:22:30 +0530

    bro_code_react_tutorial

---
+++ react_tutorial_brocode/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+import Food from "./Food.jsx";
+function App() {
+    return (
+        <>
+            <Header />
+            <Food />
+            <Footer />
+        </>
+    );
+}
+export default App;
---
+++ react_tutorial_brocode/my-react-app/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food() {
+    const food1 = "Santra";
+    const food2 = "Seb";
+    return (
+        <ul>
+            <li>Oranges</li>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+export default Food;
