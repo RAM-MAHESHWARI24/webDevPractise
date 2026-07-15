e544a751e78ae83f226ebbf982e5d6599bd42c23
Hemonesh Maheshwari <ram123lion24@gmail.com>
2026-07-15 15:04:54 +0530

    installed react-router-dom

--- login_registration_code_with_yosuf/frontend/src/App.jsx
+++ login_registration_code_with_yosuf/frontend/src/App.jsx
@@ -1,12 +1,19 @@
 // import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
 
   return (
-    <>
-      <div>Welcome!!!</div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- login_registration_code_with_yosuf/frontend/src/pages/Register.jsx
+++ login_registration_code_with_yosuf/frontend/src/pages/Register.jsx
@@ -1,36 +1,47 @@
-
+import { Link } from "react-router-dom";
+import { useState } from "react"
 const Register = () => {
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+        email: '',
+        firstName: '',
+        lastName: ''
+    });
+    const handleChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    }
     return (
         <div>
             <div>
                 <form >
                     <div>
                         <label htmlFor="username">Username</label>
-                        <input type="text" placeholder="Username" />
+                        <input type="text" placeholder="Username" name="username" />
                     </div>
 
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" name="password" />
                     </div>
 
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Email" name="email" />
                     </div>
 
                     <div>
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text" placeholder="First Name" />
+                        <input type="text" placeholder="First Name" name="firstName" />
                     </div>
 
                     <div>
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" placeholder="Last Name" />
+                        <input type="text" placeholder="Last Name" name="lastName" />
                     </div>
                     <button type="submit">Register</button>
                 </form>
-                <p>Already have an account? <a href="/login">Login</a></p>
+                <p>Already have an account? <Link to='/login'>Login</Link></p>
             </div>
         </div>
     );
